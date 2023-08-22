98704344c98ce15b9cff6d79ab1e03702f68a403
sandeep kumar mahato <sandeepkumarmahato712@gmail.com>
2023-08-22 12:15:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+var navLinks = document.getElementById("nav-links");
+function showmenu() {
+  navLinks.style.right = "0";
+}
+function hidemenu() {
+  navLinks.style.right = "-200px";
+}
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxzrjKyI3XuxrjX5thFS2B_raMa1xetbHzs-P0RidwiyKoDofrLBHR7hb3R5viPRnZP/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Thank You!";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
+
+
+/* Animation */
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
